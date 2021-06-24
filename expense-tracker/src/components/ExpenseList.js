e25d20e9142175eb36b0a2884babe49606658264
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-06-24 17:23:21 -0600

    Splitting into ExpenseList component & css

---
+++ expense-tracker/src/components/ExpenseList.js
@@ -0,0 +1,32 @@
+import './ExpenseList.css';
+import ExpenseItem from './ExpenseItem';
+
+
+function ExpenseList(props){
+    return (
+      <div className="expenses">
+        <ExpenseItem
+          title={props.expenseList[0].title}
+          amount={props.expenseList[0].amount}
+          date={props.expenseList[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenseList[1].title}
+          amount={props.expenseList[1].amount}
+          date={props.expenseList[1].date}
+        ></ExpenseItem>{" "}
+        <ExpenseItem
+          title={props.expenseList[2].title}
+          amount={props.expenseList[2].amount}
+          date={props.expenseList[2].date}
+        ></ExpenseItem>{" "}
+        <ExpenseItem
+          title={props.expenseList[3].title}
+          amount={props.expenseList[3].amount}
+          date={props.expenseList[3].date}
+        ></ExpenseItem>
+      </div>
+    );
+}
+
+export default ExpenseList;
