4111808c520a7a7b08a65784f8e16e34717332f5
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-06-24 13:36:30 -0600

    Writing more complex JSX Code

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,6 +1,22 @@
 //A component in react is just a JavaScript function
+//You can only have 1 root element per return statement or per jsx code snippet
+/* This is not allowed!
+return <div>Date</div>
+    <div><h2>Title</h2><div>Amount</div>
+    </div>
+    You can enclose it all in a single div
+*/
 function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+    return (
+      <div>
+        <div>June 24th 2021</div>
+        <div>
+          <h2>Puro Pollo</h2>
+          <div>$120</div>
+        </div>
+      </div>
+    );
+    
 }
 
 //To use a component we need to export it (Use App.js)
