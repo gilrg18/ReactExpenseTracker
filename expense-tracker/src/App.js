69efe7868458741c44a60417ca6024df45a41bdb
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-06-24 13:27:53 -0600

    Building a First Custom Component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,12 @@
+//To use our ExpenseItem REACT component in the components folder
+import ExpenseItem from "./components/ExpenseItem";
+//1 file per component
+//App will be my ROOT component
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ expense-tracker/src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+//A component in react is just a JavaScript function
+function ExpenseItem(){
+    return <h2>Expense Item!</h2>
+}
+
+//To use a component we need to export it (Use App.js)
+export default ExpenseItem;
